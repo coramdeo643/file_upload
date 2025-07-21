62cd931c82325ad6bc458b2608b29ac81fd86bd9
coramdeo643 <coramdeo643@kakao.com>
2025-07-21 15:45:10 +0900

    250721 ProfileUploadService

--- src/main/java/com/tenco/blog/user/UserController.java
+++ src/main/java/com/tenco/blog/user/UserController.java
@@ -18,6 +18,7 @@ public class UserController {
 
 	private static final Logger log = LoggerFactory.getLogger(UserController.class);
 	private final UserService userService;
+	private final ProfileUploadService profileUploadService;
 
 	/**
 	 * 회원 정보 수정 화면 요청
@@ -25,8 +26,10 @@ public class UserController {
 	@GetMapping("/user/update-form")
 	public String updateForm(Model model, HttpSession session) {
 
+		// mustache 파일에서 sessionUser 키값 출력 하는 코드 있음
 		User sessionUser = (User) session.getAttribute("sessionUser");
 		User user = userService.findById(sessionUser.getId());
+		// 모델에서 관리하는 user 키값으로 mustache에
 		model.addAttribute("user", user);
 		return "user/update-form";
 	}
@@ -90,13 +93,30 @@ public class UserController {
 		return "redirect:/";
 	}
 
-	@PutMapping("/user/uploads-profile-image")
-	public String uploadsProfileImage(@RequestParam(name = "profileImage") MultipartFile multipartFile, HttpSession session) {
-		return "redirect://user/update-form";
+	@PostMapping("/user/upload-profile-image")
+	public String uploadProfileImage(@RequestParam(name = "profileImage")MultipartFile multipartFile,
+									 HttpSession session) {
+
+		// 인증 검사는 인터 셉터에서 처리
+		User sessionUser = (User) session.getAttribute(Define.SESSION_USER);
+
+		// 유효성 검사 (파일 유효성 검사)
+		UserRequest.ProfileImageDTO profileImageDTO = new UserRequest.ProfileImageDTO();
+		profileImageDTO.setProfileImage(multipartFile);
+		profileImageDTO.validate();
+
+		// 서비스에게 일 위임( DB 저장 및 실제 파일 생성 까지)
+		User updateUser = userService.uploadProfileImage(sessionUser.getId(), multipartFile);
+
+		// 세션값에 새로운 값을 재 갱신 해주어야 한다. (세션 재 갱신 처리)
+		session.setAttribute(Define.SESSION_USER, updateUser);
+
+		// 업로드 로직 구현 시작 ....
+		return "redirect:/user/update-form";
 	}
 
 	@PutMapping("/user/delete-profile-image")
 	public String deleteProfileImage(HttpSession session) {
-		return "redirect://user/update-form";
+		return "redirect:/user/update-form";
 	}
 }
